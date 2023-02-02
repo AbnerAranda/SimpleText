d2581823403bdaa70facb902c19906b50ba67daf
Abner Alberto Aranda Nassar <67808438+AbnerAranda@users.noreply.github.com>
2023-02-02 11:31:18 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "simpletext.h"
+#include "simpletext2.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    SimpleText w;
+    SimpleText2 w;
     w.show();
     return a.exec();
 }
